9cd3fa7c08daa300678e20d350db7878e1f08612
steve <nguyenhongquan.work@gmail.com>
2025-09-22 15:30:40 +0700

    fix DisocveryApplication

--- services/discovery/src/main/java/com/myshop/discovery/DiscoveryApplication.java
+++ services/discovery/src/main/java/com/myshop/discovery/DiscoveryApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
